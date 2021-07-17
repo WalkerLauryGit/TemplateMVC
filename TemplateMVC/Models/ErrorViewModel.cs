6a2b23e129a4b57709ad80b5ddf841cf014a6ec2
WalkerLauryGit <47998525+WalkerLauryGit@users.noreply.github.com>
2021-07-17 09:48:34 -0500

    Add project files.

---
+++ TemplateMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
